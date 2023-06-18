71fbc3b686d7e768a973c170443a4b09f1665a4b
sumil <sumilspdr@gmail.com>
2023-06-19 01:39:51 +0530

    3rd code

--- MyJavaPrograms/src/patterns/Question03.java
+++ MyJavaPrograms/src/patterns/Question03.java
@@ -1,7 +1,7 @@
 package patterns;
 
-public class Question01 {
+public class Question03 {
 	public static void main(String[] args) {
-		System.out.println("bye");
+		System.out.println("tata");
 	}
 }
